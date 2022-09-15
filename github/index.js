b57932cbb3df4e54b5a069bfe9be60d6118f9d34
Dragos Catalin <dragoscv12@gmail.com>
2022-09-16 00:51:06 +0300

    require

--- github/index.js
+++ github/index.js
@@ -1,6 +1,5 @@
-import { app } from '../firebase.config.js'
-import { getFirestore } from "firebase/database";
-
+const { app } = require('../firebase.config.js')
+const { getFirestore } = require("firebase/database")
 const db = getFirestore(app);
 const docRef = await addDoc(collection(db, "videos"), {
     dateAdded: new Date(),
