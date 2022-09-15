5a212bd0e7e523472fbb897df3b603936b6a24a7
Dragos Catalin <dragoscv12@gmail.com>
2022-09-16 02:47:18 +0300

    yesss

--- github/index.js
+++ github/index.js
@@ -4,6 +4,13 @@ async function addToDb() {
         name: "Tokyo",
         country: "Japan"
     }
-    await fetch("https://savetiktok.vercel.app/api/categories").then(res => res.json()).then(res => console.log(res))
+    const qData = {
+        source_id: '7',
+        templates: 'full',
+        listing_display_id: '2',
+        page_limit: '100',
+        url: '/laptopuri/c'
+    }
+    await fetch(`https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}`).then(res => res.json()).then(res => console.log(res.metadata))
 }
 addToDb()
