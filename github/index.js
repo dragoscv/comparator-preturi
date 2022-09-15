9f450fd5c3410147c9483e8daf34b7671a7536fe
Dragos Catalin <dragoscv12@gmail.com>
2022-09-16 00:49:21 +0300

    new update

--- github/index.js
+++ github/index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+import { app } from '../firebase.config.js'
+import { getFirestore } from "firebase/database";
+
+const db = getFirestore(app);
+const docRef = await addDoc(collection(db, "videos"), {
+    dateAdded: new Date(),
+});
+
+console.log("Doc id: " + docRef.id);
