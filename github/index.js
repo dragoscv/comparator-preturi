b5e2d027d73d82bc0f98deaa25acb425cd047d71
Dragos Catalin <dragoscv12@gmail.com>
2022-09-16 00:55:55 +0300

    async

--- github/index.js
+++ github/index.js
@@ -1,8 +1,10 @@
 const { app } = require('../firebase.config.js')
 const { getFirestore } = require("firebase/database")
 const db = getFirestore(app);
-const docRef = await addDoc(collection(db, "videos"), {
-    dateAdded: new Date(),
-});
-
-console.log("Doc id: " + docRef.id);
+async function addToDb() {
+    const docRef = await addDoc(collection(db, "videos"), {
+        dateAdded: new Date(),
+    });
+    console.log("Document written with ID: ", docRef.id);
+}
+addToDb()
