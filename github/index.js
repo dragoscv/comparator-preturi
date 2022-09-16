8d6ab2779c9e873bc4f33a8c54858594af56cfbb
Dragos Catalin <dragoscv12@gmail.com>
2022-09-16 03:54:46 +0300

    do it online

--- github/index.js
+++ github/index.js
@@ -4,13 +4,36 @@ async function addToDb() {
         name: "Tokyo",
         country: "Japan"
     }
-    const qData = {
-        source_id: '7',
-        templates: 'full',
-        listing_display_id: '2',
-        page_limit: '100',
-        url: '/laptopuri/c'
-    }
-    await fetch(`https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}`).then(res => res.json()).then(res => console.log(res.metadata))
+
+    const categories = [
+        { name: "laptopuri" },
+        { name: "desktop-pc" },
+    ]
+
+    new Promise(async (resolve, reject) => {
+        categories.forEach(async (category) => {
+            const qData = {
+                source_id: '7',
+                templates: 'full',
+                listing_display_id: '2',
+                page_limit: '100',
+                url: `/${category.name}`,
+            }
+            const initial = await fetch(`https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}/c`).then(res => res.json()).then(res => res)
+            const itemsCount = initial.data.pagination.items_count
+            const pages = Math.ceil(itemsCount / qData.page_limit)
+            new Promise(async (resolve, reject) => {
+                for (let i = 0; i < pages; i++) {
+                    const page = i == 0 ? '' : `/p${i + 1}`
+                    console.log(page)
+                    const pageData = await fetch(`https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}${page}/c`).then(res => res.json()).then(res => console.log(res.metadata))
+                    resolve(pageData)
+                }
+            })
+            console.log(pages)
+            console.log(itemsCount)
+        })
+        resolve();
+    })
 }
 addToDb()
