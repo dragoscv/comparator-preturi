52da4f81a146034589bc3992ac26c8a1881d5c1f
Dragos Catalin <dragoscv12@gmail.com>
2022-09-16 04:03:33 +0300

    cu link

--- github/index.js
+++ github/index.js
@@ -19,13 +19,17 @@ async function addToDb() {
                 page_limit: '100',
                 url: `/${category.name}`,
             }
-            const initial = await fetch(`https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}/c`).then(res => res.json()).then(res => res)
+            const initialUrl = `https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}/c`
+            console.log(initialUrl)
+            const initial = await fetch(initialUrl).then(res => res.json()).then(res => res)
             const itemsCount = initial.data.pagination.items_count
             const pages = Math.ceil(itemsCount / qData.page_limit)
-                for (let i = 0; i < pages; i++) {
-                    const page = i == 0 ? '' : `/p${i + 1}`
+                for (let i = 1; i < pages; i++) {
+                    const page = i == 1 ? '' : `/p${i}`
                     console.log(page)
-                    const pageData = await fetch(`https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}${page}/c`).then(res => res.json()).then(res => console.log(res.metadata))
+                    const pageUrl = `https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}${page}/c`
+                    console.log(pageUrl)
+                    const pageData = await fetch(pageUrl).then(res => res.json()).then(res => console.log(res.metadata))
                 }
             console.log(pages)
             console.log(itemsCount)
