9f4352b7f01efb6cea79864ea10a3c3f22d749a5
Dragos Catalin <dragoscv12@gmail.com>
2022-09-16 03:58:32 +0300

    oare?

--- github/index.js
+++ github/index.js
@@ -10,7 +10,7 @@ async function addToDb() {
         { name: "desktop-pc" },
     ]
 
-    new Promise(async (resolve, reject) => {
+
         categories.forEach(async (category) => {
             const qData = {
                 source_id: '7',
@@ -22,18 +22,13 @@ async function addToDb() {
             const initial = await fetch(`https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}/c`).then(res => res.json()).then(res => res)
             const itemsCount = initial.data.pagination.items_count
             const pages = Math.ceil(itemsCount / qData.page_limit)
-            new Promise(async (resolve, reject) => {
                 for (let i = 0; i < pages; i++) {
                     const page = i == 0 ? '' : `/p${i + 1}`
                     console.log(page)
                     const pageData = await fetch(`https://www.emag.ro/search-by-url?source_id=${qData.source_id}&templates%5B%5D=${qData.templates}&listing_display_id=${qData.listing_display_id}&page%5Blimit%5D=${qData.page_limit}&url=${qData.url}${page}/c`).then(res => res.json()).then(res => console.log(res.metadata))
-                    resolve(pageData)
                 }
-            })
             console.log(pages)
             console.log(itemsCount)
         })
-        resolve();
-    })
 }
 addToDb()
